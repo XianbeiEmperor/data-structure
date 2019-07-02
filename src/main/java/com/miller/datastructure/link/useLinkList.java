cf3446c9eb2003a284c277ca3b6a4d78dc61f836
xianbeiEmperor <1099795101@qq.com>
2019-07-02 09:51:47 +0800

    修改delete方法！

--- src/main/java/com/miller/datastructure/link/useLinkList.java
+++ src/main/java/com/miller/datastructure/link/useLinkList.java
@@ -1,5 +1,7 @@
 package com.miller.datastructure.link;
 
+import java.util.LinkedList;
+
 public class useLinkList {
 
     public static void main(String[] args) {
@@ -8,5 +10,15 @@ public class useLinkList {
          oneList.insertFirst(2,"关羽",112336);
          oneList.insertFirst(3,"刘备",122356);
          oneList.displayList();
+         System.out.println("找到的某个元素： " + oneList.findOne(1).toString());
+         oneList.delete(1);
+         oneList.displayList();
+
+        //java中已经实现的链表
+        LinkedList<String> linkedList = new LinkedList<>();
+        linkedList.add("fengzhenbiao");
+        linkedList.add("miller");
+        boolean ret =  linkedList.contains("miller");
+        System.out.println(ret);
     }
 }
